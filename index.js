4e7722dc9d886f51ad6c65593272fcaf4322b96a
Moosadeveloper <janmoosa77@gmail.com>
2024-03-26 20:21:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var caseQty ="";
+
+
+caseQty = 144;
+console.log(caseQty);
+
+
+var num = "9";
+var moosa=num+2;
+
+console.log(moosa);
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
